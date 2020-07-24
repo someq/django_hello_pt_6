49db3515cdca27368a9b1ad76e0efceacdbc121c
Farid <thesomeq@gmail.com>
2020-07-24 21:04:58 +0600

    Добавлены имена для всех url на сайте.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from webapp.models import Article, STATUS_CHOICES
 from django.http import HttpResponseNotAllowed
+from django.urls import reverse
+
+from webapp.models import Article, STATUS_CHOICES
 
 
 def index_view(request):
@@ -32,7 +34,8 @@ def article_create_view(request):
         status = request.POST.get('status')
         article = Article.objects.create(title=title, text=text, author=author, status=status)
 
-        return redirect(f'/article/{article.pk}/')
+        # redirect_url = reverse('article_view', kwargs={'pk': article.pk})
+        return redirect('article_view', pk=article.pk)
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
