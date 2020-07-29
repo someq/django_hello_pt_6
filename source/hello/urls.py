6ce7ad0993c3d393ce9b4c56f276829ea16b8913
Farid <thesomeq@gmail.com>
2020-07-29 20:26:34 +0600

    Добавлено удаление статьи

--- source/hello/urls.py
+++ source/hello/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from webapp.views import index_view, article_create_view, article_view, \
-    article_update_view
+    article_update_view, article_delete_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('article/<int:pk>/', article_view, name='article_view'),
     path('articles/add/', article_create_view, name='article_create'),
     path('article/<int:pk>/update/', article_update_view, name='article_update'),
+    path('article/<int:pk>/delete/', article_delete_view, name='article_delete'),
 ]
