c49cf69be9fd5a1c3a0e4298ea318cbeb4d6acf2
Farid <thesomeq@gmail.com>
2020-08-08 19:31:31 +0600

    Добавлен вывод тегов на странице статьи и выбор тегов в форме при создании и редактировании статьи.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import STATUS_CHOICES, Article
+from .models import STATUS_CHOICES, Article, Tag
 
 default_status = STATUS_CHOICES[0][0]
 
@@ -21,6 +21,7 @@ class ArticleForm(forms.Form):
                                      widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     # для полей типа DateField
     # publish_at = forms.DateField(..., widget=forms.DateInput(attrs={'type': 'date'}))
+    tags = forms.ModelMultipleChoiceField(required=False, label='Теги', queryset=Tag.objects.all())
 
 
 class CommentForm(forms.Form):
